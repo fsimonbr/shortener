18d363d23dfa29c6801fcbd4360d45a783d20cee
Simon <fsimonbr@gmail.com>
2018-06-21 16:49:49 -0300

    arquitetura

--- shortener-api/routes/index.js
+++ shortener-api/routes/index.js
@@ -1,31 +1,81 @@
 module.exports = function (app, nus) {
   var opts = app.get('opts')
     , http = require('http')
-    , api = require('./api.js')(app, nus);
+    , router = require('express').Router();
 
-  // api routes
-  app.use('/api/v1', api);
-
-  // catch 404 and forwarding to error handler
-  app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
-
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    if (/^\/api\/v1/.test(req.originalUrl)) {
-      res.json({
-        status_code: err.status || 500,
-        status_txt: http.STATUS_CODES[err.status] || ''
+  router.route('/shorten')
+    .post(function (req, res) {
+      nus.shorten(req.body['long_url'],req.body['brand'], req.body['start_date'], req.body['end_date'], req.body['c_new'], function (err, reply) {
+        if (err) {
+          jsonResponse(res, err);
+        } else if (reply) {
+          reply.short_url = opts.url.replace(/\/$/, '') + '/' + req.body['brand'] +'/'+ reply.hash;
+          jsonResponse(res, 200, reply);
+        } else {
+          jsonResponse(res, 500);
+        }
       });
-    } else {
-      res.render('error', {
-        code: err.status || 500,
-        message: err.message,
-        error: false
+    });
+
+  router.route('/all')
+    .get(function (req, res) {
+      nus.all( req,res,function (err, reply) {
+        if (err) {
+          jsonResponse(res, err);
+        } else if (reply) {
+		jsonResponse(res, 200, reply);	
+        } else {
+          jsonResponse(res, 500);
+        }
       });
-    }
-  });
+    });
+
+  router.route('/:brand/all')
+    .get(function (req, res) {
+      nus.allBrand(req.params.brand,req,res,function (err, reply) {
+        if (err) {
+          jsonResponse(res, err);
+        } else if (reply) {
+		jsonResponse(res, 200, reply);	
+        } else {
+          jsonResponse(res, 500);
+        }
+      });
+    });
+
+  router.route('/delete')
+    .post(function (req, res) {
+      nus.deletekey(req.body['short_url'], function (err, reply) {
+        if (err) {
+          jsonResponse(res, err);
+        } else if (reply) {
+          jsonResponse(res, 200, reply);
+        } else {
+          jsonResponse(res, 500);
+        }
+      },'');
+    });
+
+  router.route('/details/:brand/:short_url')
+    .get(function (req, res) {
+      nus.expand(req.params.short_url, function (err, reply) {
+        if (err) {
+          jsonResponse(res, err);
+        } else if (reply) {
+           jsonResponse(res, 200, reply);
+        } else {
+          jsonResponse(res, 500);
+        }
+      },false,req.params.brand);
+    });
+
+  function jsonResponse (res, code, data) {
+    data = data || {};
+    data.status_code = (http.STATUS_CODES[code]) ? code : 503,
+    data.status_txt = http.STATUS_CODES[code] || http.STATUS_CODES[503]
+
+    res.status(data.status_code).json(data)
+  }
+
+  app.use('/api/v1', router);
 };
